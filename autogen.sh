ed8b44067d2489771f4ee5eff9f63313d4a2a8c4
Ehab Saleh <ehab.saleh@lrz.de>
2026-01-15 10:53:52 +0100

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,10 @@
 rm -rf build
 cmake -S . -B build
 cmake --build build
-cmake --install build --prefix $HOME/nom
+cmake --install build --prefix $PWD
 
 rm -rf build
-echo "microbenchmark is compiled on $PWD/example/"
-
+echo;
+echo "microbenchmark is compiled on $PWD"
+echo;
 echo "To run the microbenchmakr use mpirun -np X ./executable_file"
